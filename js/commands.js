8f14fd83549ed25bf467d2571854d63a6193a0bc
CholidFajar <cholidfajar15@gmail.com>
2023-06-16 00:06:37 +0700

    first commit untuk portfolio cholid

---
+++ js/commands.js
@@ -0,0 +1,106 @@
+var facebook = 'https://web.facebook.com/profile.php?id=100053132540488';
+var linkedin = 'https://www.linkedin.com/in/cholidfajar/';
+var instagram = 'https://www.instagram.com/_kholidfajar/';
+var github = 'https://github.com/cholid15';
+var email = 'mailto:cholidfajar15@gmail.com';
+
+aboutme = [
+  '<br>',
+  "<span class='underline'>Hai, saya Cholid Fajar Supardi👋</span>",
+  '<span> biasanya dipanggil <span class="underline">fajar</span>.</span>',
+  '<br>',
+  '<li> 👨‍🎓 Saat ini saya adalah mahasiswa semester akhir angkatan 2019, mengambil program studi Teknik Informatika dari 🏫 Unversitas Islam Sultan Agung </li>',
+  '<li>🌱 Bidang minat saya meliputi</li>',
+  `<pre class="whitespace-pre-wrap">
+        - Front End Web Developer </pre>`,
+  `<li><pre class="whitespace-pre-wrap">
+  😁 Saya suka belajar hal baru dan menggunakannya untuk 
+   meningkatkan keterampilan saya.
+   Saya  menggunakan Bootsrap CSS saat 
+   membangun situs web (proyek) saya.
+   Anda dapat melihat lebih lanjut tentang proyek-proyek 
+   yang telah saya bangun menggunakan perintah "projects".
+   Anda dapat menjelajahi situs web saya untuk mengetahui lebih 
+   lanjut tentang saya.`,
+  `<li><pre class="whitespace-pre-wrap">
+  🔎 Lihat profil GitHub saya untuk melihat proyek-proyek 
+   saya.
+   *Gunakan perintah "github" untuk melihat profil GitHub saya.`,
+  `<li>📧 Hubungi saya di <a href= "https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=cholidfajar15@gmail.com">cholidfajar15@gmail.com</a>`,
+  '<br>',
+];
+
+social = [
+  '<br>',
+  'facebook       <a href="' + facebook + '" target="blank">facebook/kholidfajar' + '</a>',
+  'linkedin       <a href="' + linkedin + '" target="blank">linkedin/cholidfajar' + '</a>',
+  'instagram      <a href="' + instagram + '" target="blank">instagram/_kholidfajar' + '</a>',
+  'github         <a href="' + github + '" target="blank">github.com/cholid15' + '</a>',
+
+  '<br>',
+];
+
+projects = [
+  '<br>',
+  `<a href="https://github.com/cholid15/Resto-Debetjak" target="blank"><span class='underline'>New Debetjak Resto</span>`,
+  '<br>',
+  `<pre class="indent-8 whitespace-pre-wrap">Sebuah website Landing Page restoran, New Debetjak Resto adalah restoran yang bertemakan betjak dan terdapat banyak fasilitas seperti karaoke dan menu makanan dan minuman yang nikmat serta lokal.
+
+Project bisa lihat disini <a href="https://betjakresto.netlify.app/" target="blank">https://betjakresto.netlify.app/</a>
+
+Di buat dengan
+
+    *HTML
+    *Bootstrap CSS
+    *Javascript
+`,
+
+  '<br>',
+
+  `<a href="https://github.com/cholid15/travelku" target="blank"><span class='underline'>Website TravelKuy</span>`,
+  '<br>',
+  `<pre class="indent-8 whitespace-pre-wrap">Website Travelkuy adalah Website Landing Page yang bertemakan Travel, cocok sebagai anak muda yang ingin liburan. 
+
+  Project bisa lihat disini <a href="https://travelkuy1.netlify.app/" target="blank">https://travelkuy1.netlify.app/</a>
+
+Built With
+
+    *HTML 5
+    *Boostrap 4
+    *Javascript
+`,
+
+  '<br>',
+
+  `<a href="https://github.com/cholid15/kedai-kopi-kenangan-senja" target="blank"><span class='underline'>Kedai Kopi Kenangan Senja</span>`,
+  '<br>',
+  `<pre class="indent-8 whitespace-pre-wrap">Website Kopi kenangan senja adalah website latihan yang saya dapatkan dari Channel Youtube 'Web Programming Unpas'
+
+  Project bisa lihat disini <a href="https://kopisenja.netlify.app/" target="blank">https://kopisenja.netlify.app/</a>
+
+Built With
+
+    *HTML 5
+    *CSS 
+    *Javascript
+`,
+];
+
+help = [
+  `<br><pre class="whitespace-pre-wrap">
+  <span class="command">aboutme</span>    
+   <span class="command">↳</span>&nbsp;Siapa Saya??
+  <span class="command">social</span>     
+   <span class="command">↳</span>&nbsp;Daftar Media Sosial.
+  <span class="command">projects</span>   
+   <span class="command">↳</span>&nbsp;Lihat Projets.
+  <span class="command">email</span>      
+   <span class="command">↳</span>&nbsp;Kirim Email.
+  <span class="command">history</span>    
+   <span class="command">↳</span>&nbsp;Lihat Riwayat Perintah.
+  <span class="command">help</span>       
+   <span class="command">↳</span>&nbsp;Tampilkan perintah Help.
+  <span class="command">clear</span>
+   <span class="command">↳</span>&nbsp;Clear terminal.</pre><br>`,
+];
+banner = [`<div class="pt-2"><span class="text-[#7d82d7db] ">Welcome to my portfolio! — Ketik <span class="command text-[#75e1e7]">help</span> untuk melihat daftar perintah.</span></div>`];
